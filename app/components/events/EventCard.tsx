167a143cabf53a3e4eda270c4ff71043235b506f
wael <wael.22@live.fr>
2023-12-02 11:43:59 +0100

    aaaa

--- app/components/events/EventCard.tsx
+++ app/components/events/EventCard.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { formatPrice } from "@/utils/formatPrice";
 import { truncateText } from "@/utils/truncateText";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -12,14 +11,14 @@ const EventCard: React.FC<EventCardProps> = ({data}) => {
   const router = useRouter();
 
   return (
-    <div onClick={() => router.push(`/event/${data.id}`)} className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2  transition hover:scale-105 text-center text-sm">
+    <div onClick={() => router.push(`/event/${data.id}`)} className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-200 rounded-sm p-2   transition hover:scale-105 text-center text-sm">
      <div className="flex flex-col items-center w-full gap-1">
        <div className="aspect-square overflow-hidden relative w-full">
         <Image src={data.images[0].image} fill className="w-full h-full object-cover" alt=""/>
         </div>
         <div className="w-full flex items-center justify-center gap-8 mt-2">
-         <Image src={data?.images[3]?.image} width={80} height={50} className=" h-full object-cover" alt=""/>
-         <Image src={data?.images[2]?.image} width={80} height={50} className=" h-full object-cover" alt=""/>
+         <Image src={data?.images[3]?.image} width={80} height={50} className="w-full h-full object-cover" alt=""/>
+         <Image src={data?.images[2]?.image} width={80} height={50} className="w-full h-full object-cover" alt=""/>
         </div>
        <div className="mt-4 font-semibold text-red-600 text-xl">{truncateText(data.name)}</div>
        <div className="">
